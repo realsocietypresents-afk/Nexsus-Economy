2615f40f8800d09d40c16cd1875d84a05b4771f7
realsocietypresents-afk <realsocietypresents@gmail.com>
2026-08-30 14:20:19 -0700

    Delete src/main directory

--- src/main/java/com/nexusuniverse/economy/shop/CustomShopEntry.java
+++
@@ -1,7 +0,0 @@
-package com.nexusuniverse.economy.shop;
-
-import org.bukkit.inventory.ItemStack;
-
-/** Buy-only by design -- these carry custom NBT from other Nexus plugins, so there's no reliable generic way to price a sell-back. */
-public record CustomShopEntry(ItemStack item, double buy) {
-}
